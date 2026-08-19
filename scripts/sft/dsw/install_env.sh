09fd4a4c3676ffe77fa97b014c1fab2a8094d39f
dlgjr <985039081@qq.com>
2026-08-19 16:29:43 +0800

    force PEFT 0.18 into NAS venv

--- scripts/sft/dsw/install_env.sh
+++ scripts/sft/dsw/install_env.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 PY="/mnt/nas/bihaoran/common_agent/envs/qwen35_swift/bin/python"
-"$PY" -m pip install "transformers==5.2.0" "peft==0.18.0" "datasets>=4.0.0" "pyarrow>=18.0.0" tensorboard
+"$PY" -m pip install --ignore-installed --no-deps "peft==0.18.0"
+"$PY" -m pip install "transformers==5.2.0" "datasets>=4.0.0" "pyarrow>=18.0.0" tensorboard
